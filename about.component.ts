5c8636a8bb0bdf51013b50b4dd3e31827a15776f
RajeshRakshan <111171348+RajeshRakshan@users.noreply.github.com>
2022-08-22 09:51:52 +0530

    1st Commit

---
+++ about.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import{Router} from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor(public router:Router) { }
+
+
+gotohome(){
+
+  this.router.navigateByUrl('/home')
+}
+gotocontact(){
+
+  this.router.navigateByUrl('/contact')
+}
+
+  ngOnInit(): void {
+    
+  }
+
+}
