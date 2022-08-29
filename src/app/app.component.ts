e604e6a00d85fad501b336829930cfeef75f37aa
RajeshRakshan <111171348+RajeshRakshan@users.noreply.github.com>
2022-08-29 15:19:27 +0530

    Form-Submit-Default-Patch-Refresh
    Form addition Submission, defaultValue, patchValue and refresh content...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,36 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'Form-New';
 
-onsubmit(userForm:NgForm){
-console.log(userForm)
+onsubmit=(userForm:NgForm)=>{
+console.log(JSON.stringify(userForm.value));
+}
+@ViewChild('userForm') uf:NgForm
+setValue(){
+  this.uf.setValue(
+    {"NameDetails":{"FirstName":"Rajesh","lastName":"RakeshJay"},
+    "UserdName":"rajeshrakshan1989@gmail.com",
+    "AddressDetails":{"Cityy":"cuddalore","ZipCode":"607002"},
+    "CheckOut":true}
+  ) 
+}
 
-}}
+setDefaultValue(){
+  this.uf.setValue(
+    {"NameDetails":{"FirstName":"","lastName":""},
+    "UserdName":"",
+    "AddressDetails":{"Cityy":"","ZipCode":""},
+    "CheckOut":false}
+  ) 
+}
+valueCorrection(){
+  this.uf.form.patchValue(
+    {"NameDetails":{"FirstName":"Rakshan","lastName":"Jayashree"}}
+  ) 
+}
+
+
+
+
+
+
+}
